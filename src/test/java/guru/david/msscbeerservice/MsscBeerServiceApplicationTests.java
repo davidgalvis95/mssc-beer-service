44d407ab9be99066676ff8c00798ecd3b05a789e
David Galvis <ivan.galvis@project44.com>
2020-08-17 21:38:00 -0500

    Initial project add. Closes #1

---
+++ src/test/java/guru/david/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.david.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
