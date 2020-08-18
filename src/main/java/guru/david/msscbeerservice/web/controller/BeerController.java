8b53383c9b0d61705195e88fa424398cab05632d
David Galvis <ivan.galvis@project44.com>
2020-08-18 07:27:48 -0500

    adding beer controller. closes #3

---
+++ src/main/java/guru/david/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,30 @@
+package guru.david.msscbeerservice.web.controller;
+
+
+import guru.david.msscbeerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping
+@RestController("api/v1/beer")
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId){
+        //todo implementation
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto){
+        //todo implementation
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+    @PutMapping("/{beerId}")
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto){
+        //todo implementation
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
