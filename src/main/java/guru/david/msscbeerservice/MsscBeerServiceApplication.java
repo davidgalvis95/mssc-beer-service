9940b205261ae0f04901eadd32c5fa69d076a333
David Galvis <ivan.galvis@project44.com>
2020-09-07 16:52:07 -0500

    Setting JMS and correcting the hibernate mysql dialect, closes #28

--- src/main/java/guru/david/msscbeerservice/MsscBeerServiceApplication.java
+++ src/main/java/guru/david/msscbeerservice/MsscBeerServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
 
 //this is a thing coming from JMS in the parent pom so this is generating some issues in the running of the service
-@SpringBootApplication(exclude = ArtemisAutoConfiguration.class )
+@SpringBootApplication//(exclude = ArtemisAutoConfiguration.class )
 public class MsscBeerServiceApplication {
 
     public static void main(String[] args) {
